fe7266e08f29dd6e5cf3e30356176287df21df5d
Johannes Plunien <plu@pqpq.de>
2015-12-25 04:26:00 +0100

    Load coveralls

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,8 @@
+require 'coveralls'
 require 'maxitest/autorun'
 require 'shoulda-context'
+
+Coveralls.wear!
 
 $LOAD_PATH.push File.expand_path("../../lib", __FILE__)
 require File.dirname(__FILE__) + '/../lib/simctl.rb'
