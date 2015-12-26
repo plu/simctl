f8f75f12cb9c43508c0cdf2dc02d8c2e4e81433f
Johannes Plunien <plu@pqpq.de>
2015-12-26 05:05:20 +0100

    Move coveralls up

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,8 @@
 require 'coveralls'
+Coveralls.wear!
+
 require 'maxitest/autorun'
 require 'shoulda-context'
-
-Coveralls.wear!
 
 $LOAD_PATH.push File.expand_path("../../lib", __FILE__)
 require File.dirname(__FILE__) + '/../lib/simctl.rb'
