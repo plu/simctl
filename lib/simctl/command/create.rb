5567e18a4959c3cce05c540994b05da6abaff3b8
Johannes Plunien <plu@pqpq.de>
2015-12-27 06:56:51 +0400

    Fix typo

--- lib/simctl/command/create.rb
+++ lib/simctl/command/create.rb
@@ -6,7 +6,7 @@ module SimCtl
       # Creates a device
       #
       # @param name [String] name of the new device
-      # @param device_type [SimCtl::DeviceTYpe] device type of the new device
+      # @param device_type [SimCtl::DeviceType] device type of the new device
       # @param runtime [SimCtl::Runtime] runtime of the new device
       def create_device(name, device_type, runtime)
         Executor.execute([COMMAND, "'#{name}'", device_type.identifier, runtime.identifier]) do |identifier|
