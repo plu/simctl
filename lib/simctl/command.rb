a0bedeb9c1b796893c8a07d600e864401e931873
Johannes Plunien <plu@pqpq.de>
2016-02-19 14:13:50 +0100

    Extract reset command

--- lib/simctl/command.rb
+++ lib/simctl/command.rb
@@ -5,6 +5,7 @@ require 'simctl/command/erase'
 require 'simctl/command/kill'
 require 'simctl/command/launch'
 require 'simctl/command/list'
+require 'simctl/command/reset'
 require 'simctl/command/shutdown'
 require 'simctl/executor'
 
@@ -17,6 +18,7 @@ module SimCtl
     include SimCtl::Command::Kill
     include SimCtl::Command::Launch
     include SimCtl::Command::List
+    include SimCtl::Command::Reset
     include SimCtl::Command::Shutdown
   end
 end
---
+++ lib/simctl/command/reset.rb
@@ -0,0 +1,25 @@
+module SimCtl
+  class Command
+    module Reset
+      # Kill, shutdown, delete and create a device
+      #
+      # @param name [String] name of the new device
+      # @param device_type [SimCtl::DeviceType] device type of the new device
+      # @param runtime [SimCtl::Runtime] runtime of the new device
+      # @return [SimCtl::Device] the device that was created
+      # @yield [exception] an exception that might happen during shutdown/delete of the old device
+      def reset_device(name, device_type, runtime)
+        begin
+          list_devices.where(name: name, os: runtime.name).each do |device|
+            device.kill!
+            device.shutdown! if device.state != 'Shutdown'
+            device.delete!
+          end
+        rescue Exception => exception
+          yield exception if block_given?
+        end
+        create_device name, device_type, runtime
+      end
+    end
+  end
+end
