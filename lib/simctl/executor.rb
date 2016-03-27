78947a33d97ac2a817c66db08d4d72410b7f5602
Johannes Plunien <plu@pqpq.de>
2016-03-27 12:46:55 +0400

    Do not merge stdout and stderr

--- lib/simctl/executor.rb
+++ lib/simctl/executor.rb
@@ -6,8 +6,8 @@ module SimCtl
     class << self
       def execute(command)
         command = command.flatten.join(' ')
-        Open3.popen2e(command) do |_, io, result|
-          output = io.read
+        Open3.popen3(command) do |stdin, stdout, stderr, result|
+          output = stdout.read
           raise StandardError.new(output) if result.value.to_i > 0
           return unless block_given?
           begin
