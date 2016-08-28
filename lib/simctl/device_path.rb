d9afd87e150512e2b1c69e93d21e98287086afe4
Johannes Plunien <plu@pqpq.de>
2016-08-28 08:26:25 +0400

    Fix looking up device path home

--- lib/simctl/device_path.rb
+++ lib/simctl/device_path.rb
@@ -3,10 +3,17 @@ module SimCtl
     attr_reader :device_plist, :global_preferences_plist, :home, :preferences_plist
 
     def initialize(udid)
-      @home = File.join(ENV['HOME'], 'Library/Developer/CoreSimulator/Devices', udid)
+      @home = File.join(device_set_path, udid)
       @device_plist = File.join(@home, 'device.plist')
       @global_preferences_plist = File.join(@home, 'data/Library/Preferences/.GlobalPreferences.plist')
       @preferences_plist = File.join(@home, 'data/Library/Preferences/com.apple.Preferences.plist')
+    end
+
+    private
+
+    def device_set_path
+      return SimCtl.device_set_path unless SimCtl.device_set_path.nil?
+      File.join(ENV['HOME'], 'Library/Developer/CoreSimulator/Devices')
     end
   end
 end
