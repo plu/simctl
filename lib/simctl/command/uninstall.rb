8ee217d120e9671f8075efbaee211289498fe1d3
Johannes Plunien <plu@pqpq.de>
2016-09-29 11:29:33 +0400

    Remove unused constant

--- lib/simctl/command/uninstall.rb
+++ lib/simctl/command/uninstall.rb
@@ -1,8 +1,6 @@
 module SimCtl
   class Command
     module Uninstall
-      COMMAND = %w[xcrun simctl uninstall]
-
       # Uninstall an app on a device
       #
       # @param device [SimCtl::Device] the device the app should be uninstalled from
