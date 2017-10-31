938063886696e6d6f4f7da96d55e515b8bb4a916
Kelvin Lu <kelvinlu@squareup.com>
2017-10-31 11:16:11 -0700

    Various behavioral changes.
    - Determine runtime version by reading `Contents/Info.plist`, instead of the
      directory name.
    - Change behavior to search each Xcodes' included runtime profiles first when
      locating the runtime root path.
    - Add renamed Xcode 9 runtime profile paths.

--- lib/simctl/xcode/path.rb
+++ lib/simctl/xcode/path.rb
@@ -11,6 +11,14 @@ module SimCtl
         def sdk_root
           File.join(HOME, 'Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk')
         end
+
+        def runtime_profiles
+          if Xcode::Version.gte? '9.0'
+            File.join(HOME, 'Platforms/iPhoneOS.platform/Developer/Library/CoreSimulator/Profiles/Runtimes/')
+          else
+            File.join(HOME, 'Platforms/iPhoneSimulator.platform/Developer/Library/CoreSimulator/Profiles/Runtimes/')
+          end
+        end
       end
     end
   end
