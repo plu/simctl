e9a4ce1a6299acca886ccc4391252afdc391bbb9
Josh Holtz <josh@rokkincat.com>
2018-03-29 08:03:16 -0500

    How about this

--- lib/simctl/xcode/version.rb
+++ lib/simctl/xcode/version.rb
@@ -1,11 +1,12 @@
 module SimCtl
   module Xcode
     class Version
-      VERSION = Gem::Version.new(`xcodebuild -version`.scan(/Xcode (\S+)/).flatten.first)
 
       class << self
         def gte?(version)
-          VERSION >= Gem::Version.new(version)
+          thing = Gem::Version.new(`xcodebuild -version`.scan(/Xcode (\S+)/).flatten.first)
+
+          thing >= Gem::Version.new(version)
         end
       end
     end
