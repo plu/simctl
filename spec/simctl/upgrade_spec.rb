fdbef0f324baf1bf0857b4521c6170a6631a02d0
Johannes Plunien <plu@pqpq.de>
2023-01-26 09:04:34 +0100

    Fix upgrade spec

--- spec/simctl/upgrade_spec.rb
+++ spec/simctl/upgrade_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 RSpec.describe SimCtl do
   describe '#upgrade' do
     it 'upgrades a device to a newer runtime' do
-      old_runtime = SimCtl.runtime(name: 'iOS 15.1')
+      old_runtime = SimCtl.runtime(name: 'iOS 15.2')
       new_runtime = SimCtl.runtime(name: 'iOS 15.2')
       device = SimCtl.reset_device 'iPhone 8', SimCtl.devicetype(name: 'iPhone 8'), old_runtime
       expect(device.runtime.version).to be == old_runtime.version
